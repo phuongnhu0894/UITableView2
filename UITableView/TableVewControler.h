5809d986bb623f5df5f0dc0a4f8957f1d1d139c7
Phuong <phuongnhu0894@gmail.com>
2015-11-16 09:38:40 +0700

    First Commit

---
+++ UITableView/TableVewControler.h
@@ -0,0 +1,13 @@
+//
+//  TableVewControler.h
+//  UITableView
+//
+//  Created by student on 11/16/15.
+//  Copyright © 2015 Techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableVewControler : UITableViewController
+
+@end
---
+++ UITableView/TableVewControler.m
@@ -0,0 +1,97 @@
+//
+//  TableVewControler.m
+//  UITableView
+//
+//  Created by student on 11/16/15.
+//  Copyright © 2015 Techmaster. All rights reserved.
+//
+
+#import "TableVewControler.h"
+
+@interface TableVewControler ()
+
+@end
+
+@implementation TableVewControler {
+    NSArray *arrayFruits;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Create TableView";
+    arrayFruits = @[@"Apple", @"Banana", @"Water melon"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [arrayFruits count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", arrayFruits[indexPath.row]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%@ - %d", arrayFruits[indexPath.row], (int)indexPath.row);
+}
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
